7049283cf6f13e252c94c7c6b9663acf9549eac9
nico1798 <trionfettinico@gmail.com>
2020-10-08 18:28:26 +0200

    auth

---
+++ lib/login/services/auth.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // sign in with email & password
+
+  // register with email & password
+
+  // sign out
+
+}
