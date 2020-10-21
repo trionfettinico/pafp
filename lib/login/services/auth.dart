7be577f1d88df65df22c901f7f60c311f2c7985e
trionfetti nico <trionfettinico@MacBook-Pro-di-trionfetti.local>
2020-10-21 22:22:43 +0200

    log in and sign in

--- lib/login/services/auth.dart
+++ lib/login/services/auth.dart
@@ -3,13 +3,28 @@ import 'package:firebase_auth/firebase_auth.dart';
 // da controllare
 
 class AuthService {
+
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // create user obj based on FirebaseUser
-
   // sign in with email & password
+  void signInAndSubmit(String _email , String _password) async {
+    try{
+      await _auth.signInWithEmailAndPassword(email: _email, password: _password);
+    }
+    catch(e){
+      print(e);
+    }
+  }
 
   // register with email & password
+  void register(String _email , String _password) async {
+    try{
+      await _auth.createUserWithEmailAndPassword(email: _email, password: _password);
+    }
+    catch(e){
+      print(e);
+    }
+  }
 
   // sign out
   void signOut() async {
