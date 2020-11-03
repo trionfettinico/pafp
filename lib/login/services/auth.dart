769878d971dea4e5476c98c7feb4256d24959120
trionfetti nico <trionfettinico@gmail.com>
2020-11-03 12:07:34 +0100

    init database

--- lib/login/services/auth.dart
+++ lib/login/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pafp/database/database.dart';
 
 // da controllare
 
@@ -6,22 +7,37 @@ class AuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future<String> signIn(String email, String password) async {
-    FirebaseUser user = (await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password)) as FirebaseUser;
+    User user = (await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password)) as User;
     return user.uid;
   }
 
-  Future<String> createUser(String email, String password) async {
-    FirebaseUser user = (await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password)) as FirebaseUser;
-    return user.uid;
+  Future<UserCredential> createUserAllievo(
+      String username, String email, String password) async {
+    UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    DatabaseService datab = DatabaseService('allievo');
+    Map<String, String> prova = {"username": username, "email": email};
+    datab.addDocument(prova);
+    return user;
   }
 
-  Future<FirebaseUser> currentUser() async {
-    FirebaseUser user = _firebaseAuth.currentUser as FirebaseUser;
+  Future<UserCredential> createUserAllenatore(
+      String username, String email, String password) async {
+    UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    DatabaseService datab = DatabaseService('allenatore');
+    Map<String, String> prova = {"username": username, "email": email};
+    datab.addDocument(prova);
+    return user;
+  }
+
+  Future<User> currentUser() async {
+    User user = _firebaseAuth.currentUser;
     return user;
   }
 
   Future<void> signOut() async {
     return _firebaseAuth.signOut();
   }
-
 }
