626783d5cd5c643723f7976fa3d5c2946751d6e0
lucapellegrini98 <l.pellegrini@apra.it>
2020-11-06 16:45:41 +0100

    tolto codice vecchio

--- lib/login/log_in.dart
+++ lib/login/log_in.dart
@@ -16,30 +16,8 @@ void LogIn(String _email,String _password) async {
 
   }
 
-class _LoginPageState extends State<LoginPage> {
-  /*Widget _backButton() {
-    return InkWell(
-      onTap: () {
-        Navigator.pop(context);
-      },
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(left: 0, top: 10, bottom: 10),
-              child: Icon(Icons.keyboard_arrow_left, color: Colors.blue),
-            ),
-            Text('Indietro',
-                style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.blue))
-          ],
-        ),
-      ),
-    );
-  }*/
+class _LoginPageState extends State<LoginPage>
+{
 
   Widget _entryField(String title, {bool isPassword = false}) {
     return Container(
