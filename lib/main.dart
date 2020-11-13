4b261e4ce58b9a9de3323aa739fa580ac171882e
lucapellegrini98 <l.pellegrini@apra.it>
2020-11-13 14:56:11 +0100

    modifiche schema progetto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'login/welcome.dart';
+import 'welcome/welcome.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
