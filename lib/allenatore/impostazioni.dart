665902edf2d06f2a5178c48b80275c7e67493a45
lucapellegrini98 <l.pellegrini@apra.it>
2020-11-13 10:17:09 +0100

    modifica layout

--- lib/allenatore/impostazioni.dart
+++ lib/allenatore/impostazioni.dart
@@ -16,52 +16,43 @@ class _ImpostazioniPageState extends State<ImpostazioniPage> {
           title: Text("Impostazioni"),
           automaticallyImplyLeading: false // rimuove il pulsante di ritorno
       ),
-      body: SingleChildScrollView(
-        child:Column(
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Card(
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    leading:Icon(Icons.lock_outline,color: Colors.blue,),
-                    title:Text("Cambia password"),
-                    trailing:Icon(Icons.keyboard_arrow_right,color: Colors.blue,),
-                    onTap: (){
-                      // indirizzamento verso pagina cambio password
+      body: _ListViewSetting(context),
+    );
+  }
 
-                    },
-                  ),
-                  ListTile(
-                    leading:Icon(Icons.notifications,color: Colors.blue,),
-                    title:Text("Notifiche"),
-                    trailing:Icon(Icons.keyboard_arrow_right,color: Colors.blue,),
-                    onTap: (){
-                      // indirizzamento verso pagina cambio password
+  Widget _ListViewSetting(BuildContext context)
+  {
+      return ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: [
+            ListTile(
+              leading:Icon(Icons.lock_outline,color: Colors.blue,),
+              title:Text("Cambia password"),
+              trailing:Icon(Icons.keyboard_arrow_right,color: Colors.blue,),
+              onTap: (){
+                // indirizzamento verso pagina cambio password
 
-                    },
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 1.0),
-                    width: double.infinity,
-                    height: 1.0,
-                    color: Colors.blue,
-                  ),
-                  ListTile(
-                    //leading:Icon(Icons.notifications,color: Colors.blue,),
-                    title:Text("Disconnetti account",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),),
-                    //trailing:Icon(Icons.keyboard_arrow_right,color: Colors.blue,),
-                    onTap: (){
-                      // disconnessione account e reindirizzamento a pagina iniziale
+              },
+            ),
+            ListTile(
+              leading:Icon(Icons.notifications,color: Colors.blue,),
+              title:Text("Notifiche"),
+              trailing:Icon(Icons.keyboard_arrow_right,color: Colors.blue,),
+              onTap: (){
+                // indirizzamento verso pagina cambio password
 
-                    },
-                  ),
-                ],
-              ),
+              },
+            ),
+            ListTile(
+              title:Text("Disconnetti account",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),),
+              onTap: (){
+                // disconnessione account e reindirizzamento a pagina iniziale
+
+              },
             ),
           ],
-        ),
-      ),
-    );
+        ).toList(),
+      );
   }
 }
