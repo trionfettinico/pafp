132262ed985463235432a89394218b2edf9c3047
nico1798 <nico.trionfetti@studenti.unicam.it>
2020-11-25 23:59:08 +0100

    log OKAY

--- lib/allievo/menu.dart
+++ lib/allievo/menu.dart
@@ -3,51 +3,51 @@ import 'package:pafp/allievo/listaschede.dart';
 import 'package:pafp/allievo/chat.dart';
 import 'package:pafp/allievo/impostazioni.dart';
 
-class MenuPage extends StatefulWidget {
+class MenuPageAllievo extends StatefulWidget {
+  final String email;
+  MenuPageAllievo({Key key, @required this.email}) : super(key: key);
   @override
-  _MenuPageState createState() => _MenuPageState();
+  _MenuPageState createState() => _MenuPageState(email);
 }
 
-class _MenuPageState extends State<MenuPage> {
-  int _selectedItemIndex=0;
+class _MenuPageState extends State<MenuPageAllievo> {
+  String email;
+  _MenuPageState(this.email);
+  int _selectedItemIndex = 0;
   final List pages = [
     ListaSchedePage(),
     ChatPage(),
     ImpostazioniPage(),
   ];
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar:Row(
+      bottomNavigationBar: Row(
         children: <Widget>[
-          buildNavBarItem(Icons.badge,0),
-          buildNavBarItem(Icons.chat,1),
-          buildNavBarItem(Icons.settings,2),
+          buildNavBarItem(Icons.badge, 0),
+          buildNavBarItem(Icons.chat, 1),
+          buildNavBarItem(Icons.settings, 2),
         ],
       ),
       body: pages[_selectedItemIndex],
     );
   }
 
-  Widget buildNavBarItem(IconData icon,int index)
-  {
+  Widget buildNavBarItem(IconData icon, int index) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         setState(() {
           _selectedItemIndex = index;
         });
       },
       child: Container(
         height: 60,
-        width: MediaQuery.of(context).size.width/4,
+        width: MediaQuery.of(context).size.width / 3,
         decoration: BoxDecoration(
           color: Colors.blue,
         ),
-        child: Icon(
-            icon,
-            color: index == _selectedItemIndex ? Colors.white : Colors.black
-        ),
+        child: Icon(icon,
+            color: index == _selectedItemIndex ? Colors.white : Colors.black),
       ),
     );
   }
